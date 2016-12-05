ec46987b67ec5e37bc79ab2609db1b0b730f3df8
gompose <peter.m12321@gmail.com>
2016-12-04 21:23:20 -0500

    Update Books.java

--- 01Library/Books.java
+++ 01Library/Books.java
@@ -4,8 +4,36 @@ public class Books{
     public String isbn;
     
     public Books() {
-        System.out.println(Not enough Information)
+        author = "";
+        title = "";
+        isbn = "";
     }
     
-    public Books(String 
+    public Books(String author, String title, String isbn) {
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    
 }
