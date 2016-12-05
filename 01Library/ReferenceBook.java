827a6e5dc8f0528a39c79fae77bea90fc24546ef
gompose <peter.m12321@gmail.com>
2016-12-04 21:50:40 -0500

    Update ReferenceBook.java

--- 01Library/ReferenceBook.java
+++ 01Library/ReferenceBook.java
@@ -1,1 +1,29 @@
-public class ReferenceBook extends
+public class ReferenceBook extends LibraryBook{
+  private String collection;
+  
+  public ReferenceBook(String author, String title, String isbn, String callNumber, String collection) {
+    this.author = author;
+    this.title = title;
+    this.isbn = isbn;
+    this.callNumber = callNumber;
+    this.collection = collection;
+  }
+  
+  public String getCollection() {
+    return collection;
+  }
+  public void checkout(String patron, String due){
+    throw new UnsupportedOperationException("Cannot checkout a Reference Book");
+  }
+  public void returned(){
+    throw new UnsupportedOperationException("Cannot return a Reference Book");
+  }
+  public String circulationStatus() {
+    return "non-circulating reference book";
+  }
+  public String toString() {
+    return "Title: " + title + "\n" + "Author: " + author + "\n" + "ISBN: " + isbn + "\n" + "CallNumber: " + callNumber + "\n" + "Collection: " + collection;
+  }
+  
+  
+}
