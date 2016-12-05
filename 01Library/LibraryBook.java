8ee29bee56d98675c88f732df5237247af4bd666
gompose <peter.m12321@gmail.com>
2016-12-04 21:31:59 -0500

    Update LibraryBook.java

--- 01Library/LibraryBook.java
+++ 01Library/LibraryBook.java
@@ -1,3 +1,20 @@
 
 
-public class LibraryBook extends 
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
+  String callNumber;
+  
+  public LibraryBook(String author, String title, String isbn, String callNumber){
+    this.author = author;
+    this.title = title;
+    this.isbn = isbn;
+    this.callNumber = callNumber;
+  }
+  public String getCallNumber() {
+    return callNumber;
+  }
+  public abstract void checkout(String patron, String due);
+  public abstract void returned();
+  public abstract String circulationStatus();
+  // NOTE: Unfinished, still need comparable and etc.
+  
+}
