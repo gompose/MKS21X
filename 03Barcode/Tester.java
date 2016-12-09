a5b34a3090d6ef0ccf279df399756b14ba9ed514
Peter Mei <peter.m12321@gmail.com>
2016-12-09 03:25:44 -0500

    Debugged Barcode

--- 03Barcode/Tester.java
+++ 03Barcode/Tester.java
@@ -1,11 +1,12 @@
 public class Tester {
     public static void main(String[] args) {
-	Barcode a = new Barcode(20483);
-	Barcode b = new Barcode(75426);
+	Barcode a = new Barcode("20483");
+	Barcode b = new Barcode("75426");
+	Barcode c = new Barcode("08451");
 
 	System.out.println(a);
 	System.out.println(b);
-	
+	System.out.println(c);
 
 
     }
