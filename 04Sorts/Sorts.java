a8498b0a60e4031bb9ebd9539548b9289638718f
Peter Mei <peter.m12321@gmail.com>
2016-12-14 03:33:03 -0500

    Re-acquired Files

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,1 +1,23 @@
 
+
+public class Sorts{
+
+    public static String name(){
+	return "06.Mei.Peter";
+    }
+
+    public static void selectionSort(int[] data) {
+	int hold = 0;
+	int indexhold;
+	int index
+	for (index = 0; index < data.length - 1; index ++) {
+	    for (index; index < data.length - 1; index ++) {
+		indexhold = index;
+		if (data[index] < hold;){
+		    hold = data[index];
+		}
+	    }
+	    index = indexhold;
+	    data[indexhold] = hold;
+	    index++;
+	    
